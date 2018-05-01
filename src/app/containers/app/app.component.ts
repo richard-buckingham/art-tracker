d8ec04a8fc4266d427600f0247f430e1ea77d305
richie.buckingham <richie.buckingham@gmail.com>
2018-05-01 17:14:23 +1000

    Reactive Store

--- src/app/containers/app/app.component.ts
+++ src/app/containers/app/app.component.ts
@@ -1,17 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+
+import { Observable } from 'rxjs/Observable';
+import { Subscription } from 'rxjs/Subscription';
+
+import { Store } from 'store';
+
+import { AuthService } from '../../../auth/shared/services/auth/auth.service';
+import { User } from '../../../auth/models/user.interface';
 
 @Component({
   selector: 'app-root',
   styleUrls: ['app.component.scss'],
-  template: `
-    <div>
-      Hello Ultimate Angular!
-      <div class="wrapper">
-        <router-outlet></router-outlet>
-      </div>
-    </div>
-  `
+  templateUrl: './app.component.html'
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit, OnDestroy {
+
+  // setup our user property
+  user$: Observable<User>;
+  userSubscription: Subscription;
+
+  constructor(private store: Store,
+              private authService: AuthService) {}
+
+  ngOnInit() {
+    // initiate the dataflow for the auth$ observable
+    this.authService.auth$.subscribe();
+
+    // As the user has been saved in the store, we can get theuser from there
+    this.user$ = this.store.select<User>('user');
+  }
+
+  ngOnDestroy() {
+    this.userSubscription.unsubscribe();
+  }
 }
--- src/auth/shared/services/auth/auth.service.ts
+++ src/auth/shared/services/auth/auth.service.ts
@@ -1,12 +1,41 @@
 import { Injectable } from '@angular/core';
-
 import { AngularFireAuth } from 'angularfire2/auth';
+
+import 'rxjs/add/operator/do';
+
+import { Store } from 'store';
+import { User } from '../../../models/user.interface';
+
 
 @Injectable()
 export class AuthService {
 
+  // subscribe to the firebase authState observable
+  // next is the firebase user
+  // NOTE. THIS NEEDS TOBE SUBSCRIBED TO, OR THE .DO CODE WILL NEVER BE CALLED.
+  auth$ = this.af.authState
+    // side effect
+    .do(next => {
+      console.log('receiving data from the firebase authState observable', next);
+
+      if(!next) {
+        console.log('user has not logged in yet. Setting the store user property to null');
+        this.store.set('user', null);
+        return;
+      }
+
+      const user: User = {
+        email: next.email,
+        uid: next.uid,
+        authenticated: true
+      }
+      console.log('user object created, and will be added to the store', user);
+      this.store.set('user', user);
+    });
+  
   constructor(
-    private af: AngularFireAuth
+    private af: AngularFireAuth,
+    private store: Store
   ) {}
 
   createUser(email: string, password: string) {
