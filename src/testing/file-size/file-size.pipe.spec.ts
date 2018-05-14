271cb2ac1d12e8ea9dd6285af5b198db939de3b9
richie.buckingham <richie.buckingham@gmail.com>
2018-05-14 17:07:13 +1000

    shallow pipe testing

--- src/testing/file-size/file-size.pipe.spec.ts
+++ src/testing/file-size/file-size.pipe.spec.ts
@@ -1,6 +1,64 @@
+import { Component } from '@angular/core';
+import { TestBed, ComponentFixture } from '@angular/core/testing';
+import { BrowserDynamicTestingModule, platformBrowserDynamicTesting } from '@angular/platform-browser-dynamic/testing';
+
+TestBed.initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting()
+);
+
 import { FileSizePipe } from './file-size.pipe';
 
 describe('FileSizePipe', () => {
+
+  describe('Shallow FileSizePipe test', () => {
+
+    @Component({
+      template: `
+        Size: {{ size | filesize:suffix }}
+      `
+    })
+    class TestComponent {
+      suffix : any;
+      size = 123456789;
+    }
+
+    let component: TestComponent;
+    let fixture: ComponentFixture<TestComponent>;
+    let el: HTMLElement;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        declarations: [
+          FileSizePipe,
+          TestComponent
+        ]
+      });
+      fixture = TestBed.createComponent(TestComponent);
+      component = fixture.componentInstance;
+      el = fixture.nativeElement;
+    });
+
+    it('should convert bytes to megabytes', () => {
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74MB');
+      component.size = 1029281;
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 0.98MB');
+    });
+
+    it('should use the default extension when not supplied', () => {
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74MB');
+    });
+
+    it('should override the extension when supplied', () => {
+      component.suffix = 'myExt';
+      fixture.detectChanges();
+      expect(el.textContent).toContain('Size: 117.74myExt');
+    });
+
+  });
 
   describe('Isolate FileSizePipe test', () => {
     
